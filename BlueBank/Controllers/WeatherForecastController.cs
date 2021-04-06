f0571db003adbe8209fda1609d00bf2527662666
Ronal Gelvez <rmgelvez@hotmail.com>
2021-04-05 23:48:42 -0500

    Prueba

--- BlueBank/Controllers/WeatherForecastController.cs
+++ BlueBank/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace BlueBank.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezingg", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
